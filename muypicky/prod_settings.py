dcc70f93d3ae7b4bce82328d25075e9d3ec96524
jack <jackthebestripper@yahoo.com>
2018-09-30 07:00:37 +0000

    Production settings

--- muypicky/prod_settings.py
+++ muypicky/prod_settings.py
@@ -11,17 +11,17 @@ sentry_sdk.init(
     integrations=[DjangoIntegration()]
 )
 
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'Justin <muypicky@gmail.com>'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Justin <muypicky@gmail.com>'
 
-# ADMINS = (
-#     ('Admin', EMAIL_HOST_USER),
-# )
-# MANAGERS = ADMINS
+ADMINS = (
+    ('Admin', EMAIL_HOST_USER),
+)
+MANAGERS = ADMINS
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'j2b_z(*4w+#)t^nz3)0n3da(tcj&3##klo73m76(x7%3z)b%85n!')
 
