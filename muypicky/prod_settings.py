4a7e6089bc87af4c3b4e0dd6f6bb4c715d85db95
jack <jackthebestripper@yahoo.com>
2018-10-03 06:12:41 +0000

    Email send settings update

--- muypicky/prod_settings.py
+++ muypicky/prod_settings.py
@@ -16,7 +16,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'Admin <admin@gmail.com>'
+DEFAULT_FROM_EMAIL = 'Serge <agounkeserge@gmail.com>'
 
 ADMINS = (
     ('Admin', EMAIL_HOST_USER),
